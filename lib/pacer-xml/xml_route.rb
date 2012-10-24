e91141d706495221bc2386290cef243ea185c1fc
Darrick Wiebe <dw@xnlogic.com>
2012-10-24 17:59:11 -0400

    Add transactions

--- lib/pacer-xml/xml_route.rb
+++ lib/pacer-xml/xml_route.rb
@@ -1,11 +1,17 @@
 module PacerXml
   module XmlRoute
     def trees(key_map = {})
-      to_route.map(route_name: 'trees') { |node| node.tree(key_map)['document'] }.route
+      to_route.map(route_name: 'trees') do |node|
+        node.tree(key_map)['document']
+      end.route
     end
 
     def import(graph, rename = {})
-      to_route.process(route_name: 'import') { |node| BuildGraph.new(graph, node, rename) }.route
+      to_route.process(route_name: 'import') do |node|
+        graph.transaction do
+          BuildGraph.new(graph, node, rename)
+        end
+      end.route
     end
   end
   Pacer::RouteBuilder.current.element_types[:xml] = [XmlRoute]
