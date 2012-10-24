7e2d415f2b56809da7f703f3d774d2331cf2fe79
Darrick Wiebe <dw@xnlogic.com>
2012-10-24 04:34:02 -0400

    Namespace

--- lib/pacer-xml/xml_route.rb
+++ lib/pacer-xml/xml_route.rb
@@ -1,10 +1,12 @@
-module XmlRoute
-  def trees(key_map = {})
-    to_route.map(route_name: 'trees') { |node| node.tree(key_map)['document'] }.route
-  end
+module PacerXml
+  module XmlRoute
+    def trees(key_map = {})
+      to_route.map(route_name: 'trees') { |node| node.tree(key_map)['document'] }.route
+    end
 
-  def import(graph, rename = {})
-    to_route.process(route_name: 'import') { |node| BuildGraph.new(graph, node, rename) }.route
+    def import(graph, rename = {})
+      to_route.process(route_name: 'import') { |node| BuildGraph.new(graph, node, rename) }.route
+    end
   end
+  Pacer::RouteBuilder.current.element_types[:xml] = [XmlRoute]
 end
-Pacer::RouteBuilder.current.element_types[:xml] = [XmlRoute]
