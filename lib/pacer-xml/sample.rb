c19a86d77290a67230177c77528f7eba1c19be75
Darrick Wiebe <dw@xnlogic.com>
2012-10-25 17:07:20 -0400

    Add sample of using and running the importer.

--- lib/pacer-xml/sample.rb
+++ lib/pacer-xml/sample.rb
@@ -1,6 +1,55 @@
+require 'set'
+
 module PacerXml
   module Sample
     class << self
+      def load(*args)
+        i = importer(*args)
+        i.run!
+        i.graph
+      end
+
+      # Sample of using the xml import function with some advanced options to
+      # clean up the resulting graph.
+      #
+      # Import can successfully be run with no options specified, but this patent
+      # xml is particularly hairy.
+      def importer(graph = nil, fn = nil, start_rule = nil, end_rule = nil)
+        html = [:abstract]
+        renames = {
+          'classification-national' => 'classification',
+          'assistant-examiner' => 'examiner',
+          'primary-examiner' => 'examiner',
+          'us-term-of-grant' => 'term',
+          'addressbook' => 'entity',
+          'document-id' => 'document',
+          'us-related-documents' => 'related-document',
+          'us-patent-grant' => 'patent-version',
+          'us-bibliographic-data-grant' => 'patent'
+        }
+        cache = { stats: true }
+        graph ||= Pacer.tg
+        graph.create_key_index :type, :vertex
+        xml_route = xml(fn, start_rule, end_rule)
+        xml_route.
+          process { print '.' }.
+          import(graph, html: html, renames: renames, cache: cache)
+      end
+
+      def xml(fn = nil, *args)
+        fn ||= a_week
+        path = download_patent_grant fn
+        Pacer.xml path, *args
+      end
+
+      def cleanup(fn = nil)
+        fn ||= a_week
+        name, week = fn.split '_'
+        Dir["/tmp/#{name}*"].each { |f| File.delete f }
+      end
+
+      private
+
       def a_week
         'ipgb20120103_wk01'
       end
@@ -17,18 +66,6 @@ module PacerXml
           end
         end
         result
-      end
-
-      def cleanup(fn = nil)
-        fn ||= a_week
-        name, week = fn.split '_'
-        Dir["/tmp/#{name}*"].each { |f| File.delete f }
-      end
-
-      def xml(fn = nil, *args)
-        fn ||= a_week
-        path = download_patent_grant fn
-        Pacer.xml path, *args
       end
     end
   end
